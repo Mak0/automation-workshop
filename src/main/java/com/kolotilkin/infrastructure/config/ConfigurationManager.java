beb64bb34f8a165bec9de9b2d611687b93b43c3d
Maksym Kolotilkin <kolotilkin@gmail.com>
2023-06-18 21:42:53 +0300

    Update ConfigurationManager.java

--- src/main/java/com/kolotilkin/infrastructure/config/ConfigurationManager.java
+++ src/main/java/com/kolotilkin/infrastructure/config/ConfigurationManager.java
@@ -30,16 +30,6 @@ public class ConfigurationManager {
 
     public Boolean isHeadlessBrowser() {
         String opt = getEnvironmentVariableOrDefault("ISHEADLESS", "false");
-        System.out.println(opt);
-
-        // DEbug info
-        Map<String, String> envMap = System.getenv();
-
-        for (String envName : envMap.keySet()) {
-            System.out.format("%s = %s%n", envName, envMap.get(envName));
-        }
-        ///////////////////
-        
         return opt.equals("true");
     }
 
